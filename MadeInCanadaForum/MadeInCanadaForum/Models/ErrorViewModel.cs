9ab89e5da0d2eb80fb6d851056bd85ad16a8ea13
alex999alex <y3d7m5@gmail.com>
2025-02-06 18:24:32 -0400

    1

---
+++ MadeInCanadaForum/MadeInCanadaForum/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MadeInCanadaForum.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

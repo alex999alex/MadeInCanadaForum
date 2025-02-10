d0b03ad8cff7bc41681998a90ac3a4f816272c05
alex999alex <y3d7m5@gmail.com>
2025-02-10 19:39:02 -0400

    S4

--- MadeInCanadaForum/MadeInCanadaForum/obj/Debug/net9.0/MadeInCanadaForum.AssemblyInfo.cs
+++ MadeInCanadaForum/MadeInCanadaForum/obj/Debug/net9.0/MadeInCanadaForum.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MadeInCanadaForum")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b65c729b847299c9a7914c2ad53026e134ea29f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+487ac0c8862a034353d720a6ee2613b5c0103851")]
 [assembly: System.Reflection.AssemblyProductAttribute("MadeInCanadaForum")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MadeInCanadaForum")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
